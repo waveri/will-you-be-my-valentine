bcef43610241da6f43561d5d95e0332d7a9c85c7
Pratik Saha <pratik05cse@gmail.com>
2026-02-13 18:41:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/valentine_day",
+  base: "/valentine_day/",
   plugins: [react()],
 });
